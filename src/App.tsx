be9215e62ac36c74a5a7efe4c462c66b23f5023b
aysebayraktar1 <aysebayraktr3@gmail.com>
2024-04-19 16:23:48 +0200

    feat: added styled-components dependency and update page title & favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>SpaceX Launches</h1>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  background: #1a1a1a;
+  background: radial-gradient(at center, #1a1a1a, #010101);
+`;
 
 export default App;
